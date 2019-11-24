a23e1d22de849a328d769321a15590d68758f577
lenival <lenival@ufrn.edu.br>
2019-11-24 12:13:55 -0300

    Added a sawtooth wave to easy view of modifications at code

--- sdft_ref.m
+++ sdft_ref.m
@@ -1,13 +1,16 @@
-x = [1 2 3 4 3 2 1 2 3 4 3 2 1 2 3 4 3 2 1 2 3 4 3 2 ];
-
+%x = [1 2 3 4 3 2 1 2 3 4 3 2 1 2 3 4 3 2 1 2 3 4 3 2 ];
+%x = randn(1,64*128);
+%x = 1:64*128;x(1:10*128) = sin(2*pi*x(1:10*128));
 %x = [1 2 3 4 1 2 3 4 1 2 3 4];
+%Fs = 10e3;t = 0:1/Fs:2;x = vco(sawtooth(2*pi*t,0.5),[0.1 0.4]*Fs,Fs);
+Fs = 10000;t = 0:1/Fs:2;x = sawtooth(2*pi*512*t,0.75);
 
 N = length(x);
-M = 4;
-L = 2;
+M = 2048;
+L = 1024;
 
 % Quantidade de janelas calculadas
-Q = (N-M)/L+1;
+Q = floor((N-M)/L+1);
 Xn_k = zeros(Q,M);
 
 
@@ -15,3 +18,11 @@ for q = 1:1:Q
     n_l = (q-1)*L+1;
     Xn_k(q,:)=fft(x(n_l:n_l+M-1));
 end
+
+%HeatMap(abs(Xn_k))
+surf(abs(Xn_k)/M)
+colormap(pink)    % change color map
+shading interp    % interpolate colors across lines and faces
+
+figure;
+plot(abs(fft(x)/N))
