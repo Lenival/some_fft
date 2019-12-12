a29675451d892b3cbace0a3349a4a10696a8c263
lenival <lenival@ufrn.edu.br>
2019-12-11 21:40:05 -0300

    Fixed the axis labels in plots

--- hdft_test.m
+++ hdft_test.m
@@ -1,17 +1,17 @@
-clear;clc
+clear%;clc
 %% Define reference signal x and your DFT
 %x = [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24];
 %x =  [1 2 3 4 3 2 1 2 3 4 3  2  1  2  3  4  3  2  1  2  3  4  3  2  1];
-%x = randn(1,2^18);
-Fs = 2^10;t = 0:1/Fs:2;x = 1+sin(2*pi*200*t);
+%x = randn(1,2^12);
+Fs = 2^15;t = 0:1/Fs:2;x = 1+sin(2*pi*200*t);
 %x = [1 2 3 4 1 2 3 4 1 2 3 4];
 %Fs = 10e3;t = 0:1/Fs:2;x = vco(sawtooth(2*pi*t,0.5),[0.1 0.4]*Fs,Fs);
 %Fs = 2^16;t = 0:1/Fs:2;x = sawtooth(2*pi*512*t,0.75);
 %Fs = 2^6;t = 0:1/Fs:2;x = sawtooth(2*pi*512*t,0.75);
 
 N = length(x);
-M = 64;            % Window length
-L = 2;            % Size of window increment
+M = 173;            % Window length
+L = 59;            % Size of window increment
 
 % How many hypothetical windows should exist before x(M)?
 q_D = floor((M-1)/L);           % Number of hypothetical windows
@@ -59,8 +59,8 @@ subplot(1,2,1)
 surf(abs(Xn_k(1:Q+q_D,:,1)))
 %title('SDFT com FFT do MATLAB')
 zlabel('X_{n}(k)')
-xlabel('n')
-ylabel('k')
+xlabel('k')
+ylabel('n')
 colormap(pink)    % change color map
 shading interp    % interpolate colors across lines and faces
 
@@ -69,8 +69,8 @@ subplot(1,2,2)
 % imagesc(abs(Xn_k(:,:,2)))
 surf(abs(Xn_k(1:Q+q_D,:,2)))
 zlabel('X_{n}(k)')
-xlabel('n')
-ylabel('k')
+xlabel('k')
+ylabel('n')
 colormap(pink)    % change color map
 %title('HDFT implementada')
 shading interp    % interpolate colors across lines and faces
@@ -78,5 +78,5 @@ shading interp    % interpolate colors across lines and faces
 figure
 mesh(abs(Xn_k_diff(1:Q+q_D,:)))
 zlabel('\epsilon')
-xlabel('n')
-ylabel('k')
+xlabel('k')
+ylabel('n')
