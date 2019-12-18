1db4bf1d7a045eb40b0dd61bf30ee8e1a21aca7e
lenival <lenival@ufrn.edu.br>
2019-12-17 23:30:51 -0300

    DIT index logic working fine

--- index_test.m
+++ index_test.m
@@ -16,18 +16,63 @@ for m = Ms
 end
 
 
-%% Twiddle factors index construction
+%% Twiddle factors and UVT decimation index construction
 %clear;
-M_test = 16; L_test = 4;
+M_test = 12; L_test = 4;
 layers = log2(L_test);
-% max_index = (2^layers-1);
 max_index = (M_test/2-1);
+
 initial_index = 0:1:max_index;
-index_table = zeros(layers,max_index);
+W_index_table = zeros(layers,max_index);
+disp('------------------------------------------')
+
+disp('------------------------------------------')
+disp('               --------                   ')
+disp('               --------                   ')
+disp('')
 
 for l = 1:1:layers
+
     for i = initial_index
-        index_table(layers+1-l,i+1) = bitshift(bitshift(i,-(l-1)),(l-1));
+disp('------------------------------------------')
+        disp([l i ])
+        W_index_table(layers+1-l,i+1) = bitshift(bitshift(i,-(l-1)),(l-1));
     end
 end
+
+% Index examples for M=16 and L=4
+%De1 0 4; 1 5;  2 6;  3 7; 8 12; 9 13; 10 14; 11 15
+%We1 +0-; +2-;  +4-;  +6-;  +0-;  +2-;   +4-;   +6-     
+%De2 0 8; 1 9; 2 10; 3 11; 4 12; 5 13;  6 14;  7 15
+%We2 +0-; +1-;  +2-;  +3-;  +4-;  +5-;   +6-;   +7-
+
+disp('------------------------------------------')
+disp('------------------------------------------')
+disp('               --------                   ')
+disp('               --------                   ')
+disp('')
+
+D_index_table = zeros(layers,max_index);
+for l = 1:1:layers
+    D_index = 0;
+    max_decimation = M_test/(2^l);
+    for i = 1:1:max_decimation
+disp('------------------------------------------')
+        disp([l i ])
+        for c = 0:1:(2^l-1)
+            D_index = D_index + 1;
+            D_index_table(layers+1-l,D_index) = max_decimation*c+(i-1);
+        end
+    end
+end
+
+
+
+
+
+
+
+
+
+
 
