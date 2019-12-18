9634a8464c12d3af5329834571390a5870b9d9dc
lenival <lenival@ufrn.edu.br>
2019-12-17 23:28:49 -0300

    Adding DIT HDFT version to tests

--- uvt_test.m
+++ uvt_test.m
@@ -1,6 +1,13 @@
 clear;clc
 %%
-Fs = 2^12;t = 0:1/Fs:2;x = sawtooth(2*pi*512*t,0.75);M = 273;L = 87;
+%x = [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24];
+%x =  [1 2 3 4 3 2 1 2 3 4 3  2  1  2  3  4  3  2  1  2  3  4  3  2  1];M = 12;L = 4;
+x = randn(1,3+2^12);M = 12;L = 4;
+% Fs = 2^17;t = 0:1/Fs:2;x = 1+sin(2*pi*200*t);M = 173;L = 59;
+%x = [1 2 3 4 1 2 3 4 1 2 3 4];
+%Fs = 10e3;t = 0:1/Fs:2;x = vco(sawtooth(2*pi*t,0.5),[0.1 0.4]*Fs,Fs);
+%Fs = 2^16;t = 0:1/Fs:2;x = sawtooth(2*pi*512*t,0.75);
+%Fs = 2^8;t = 0:1/Fs:2;x = sawtooth(2*pi*512*t,0.75);M = 16;L = 4;
 
 N = length(x);
 
@@ -8,15 +15,16 @@ N = length(x);
 q_D = floor((M-1)/L);           % Number of hypothetical windows
 X_start = mod(M-1,L)+1;         % Where should it starts
 
+% Uncoment if you want just a few bins
+% bins = 13:27;
+
+bins = 0:M-1;
+
 
 % Number of windows demanded
 Q = floor((N-M)/(L))+1;
 Xn_k = zeros(Q+q_D,M,2);
 
-% Uncoment if you want just a few bins
-% bins = 13:27;
-
-bins = 0:M-1;
 
 % calculating all the hypothetical windows before x(M)
 for n_i = 1:1:q_D
@@ -42,9 +50,9 @@ end
 
 %% HDFT function
 
-D = raw_uvt(x,N,M,L,Q,q_D,X_start,bins);
+%D = raw_uvt(x,N,M,L,Q,q_D,X_start,bins);
 %D = uvt_radix2(x,N,M,L,Q,q_D,X_start,bins);
-%D = uvt_dit_r2(x,N,M,L,Q,q_D,X_start,bins);
+D = uvt_dit_r2(x,N,M,L,Q,q_D,X_start);
 
 %% HDFT Analysis
 
